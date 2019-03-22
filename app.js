4755110ee17d46e14f9f7affcc3d514215e66f7f
AirrV <romainwcs@gmail.com>
2019-03-22 16:49:30 +0100

    introcuce random for cpu and game winner

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
 const mainDiv = document.getElementById('main');
+const elements = ["rock", "paper", "scissors"]
 
 function startNewGame(opponent) {
   document.getElementById("onePlayerScreen").innerHTML = onePlayerScreenContent(opponent)
   document.getElementById("CPUScreen").innerHTML = onePlayerScreenContent("CPU")
 }
 
-function choice(el) {
-  console.log(el)
+function game(el) {
+  if ( !el ) {
+    whoWins( randomCPU(), randomCPU() )
+  }else {
+    whoWins( el, randomCPU() )
+  }
+}
+
+function randomCPU () {
+  let id = Math.floor(Math.random() * elements.length)
+  return elements[id]
+}
+
+function whoWins (str1, str2)  {
+  let result
+  if (str1 === str2){
+    result = 'Deuce'
+  } else if (( str1 === 'rock' && str2 === 'scissors' )
+      || ( str1 === 'scissors' && str2 === 'paper' )
+      || ( str1 === 'paper' && str2 === 'rock' )) {
+    result = 'P1 wins'
+  } else {
+    result = 'CPU wins'
+  }
+  document.getElementById("winner").innerHTML = result;
 }
 
 const onePlayerScreenContent = name => {
-  let disabled; 
-  name.substring(0,3) === "CPU" ? disabled = "disabled" : disabled = null
+  let disabled;
+  name.substring(0,3) === "CPU" ? disabled = "disabled" : disabled = null;
+  name.substring(0,4) === "CPU2" ? visibility = "visible" : visibility = "hidden";
 
   return (`<div>
     <p>Name of the player : ${name}</p>
     <p>Choice</p>
     <button id='rock' name='rock' ${disabled} onclick="game('rock')">Rock</button>
     <button id='paper' name='paper' ${disabled} onclick="game('paper')">Paper</button>
-    <button id='scissors' name='scissors' ${disabled} onclick="game('scissors')">Scissors</>
+    <button id='scissors' name='scissors' ${disabled} onclick="game('scissors')">
+      Scissors
+    </button>
+    <button id='random' name='random' style="visibility: ${visibility}" onclick="game()">
+      Random
+    </button>
   </div>`
-  )
+  );
 }
 
 const homePage = `<div>
@@ -29,6 +59,7 @@ const homePage = `<div>
     <button id="cpuVsCpu">CPU VS CPU</button>
     <div id="onePlayerScreen"></div>
     <div id="CPUScreen"></div>
+    <div id="winner"></div>
   </div>`
 
 document.addEventListener("DOMContentLoaded", () => {
